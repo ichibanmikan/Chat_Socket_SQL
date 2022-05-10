33d5b3293bccb0ab806c7f485d8d109a187b55bb
ichibanmikan <ichibanmikans@gmail.com>
2022-05-10 18:09:23 +0800

    丰富了一些注释

--- src/Server/SocketServer.java
+++ src/Server/SocketServer.java
@@ -8,9 +8,9 @@ import java.net.*;
 import java.util.*;
 
 public class SocketServer{
-    public static List<String> ListOnlineUser=new ArrayList<String>();
-    static ServerSocket server = null;
-    static Socket socket = null;
+    public static List<String> ListOnlineUser=new ArrayList<String>(); //这个list存储所有在线用户的用户名
+    static ServerSocket server = null; //表示本机的服务器
+    static Socket socket = null; //线程
     static List<Socket> list = new ArrayList<Socket>();  // 存储客户端
 
     public static Boolean addUserOnline(String userName){
@@ -21,15 +21,15 @@ public class SocketServer{
         ListOnlineUser.add(userName);
         System.out.println(ListOnlineUser.size());
         return true;
-    }
+    } //如果已经在线就不能登录 不在线就可以登录
 
     public static Boolean isTheUserOnline(String userName){
         return ListOnlineUser.contains(userName);
-    }
+    } //当前用户是否在线
 
     public static void rmvNowUser(String userName){
         ListOnlineUser.remove(userName);
-    }
+    }//当客户端下线了，就把它对应的用户名从上线用户表里删除
 
     public static void main(String[] args) {
         try {
@@ -43,8 +43,8 @@ public class SocketServer{
                 list.add(socket);  // 添加当前客户端到列表
                 // 在服务器端对客户端开启相应的线程
                 System.out.println(socket.toString());
-                ServerThread readAndPrint = new ServerThread(socket);
-                readAndPrint.start();
+                ServerThread readAndPrint = new ServerThread(socket); //循环等待客户端，给每一个客户端分配一个线程
+                readAndPrint.start(); //Thread类的特点 start就是表示线程开始，调用run()函数
             }
         } catch (IOException e1) {
             e1.printStackTrace();  // 出现异常则打印出异常的位置
